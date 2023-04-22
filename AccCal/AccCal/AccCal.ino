6013b64383b775fcf86e7bc571879f42a6f1da11
AsekaAttanayake <129642730+AsekaAttanayake@users.noreply.github.com>
2023-04-22 23:19:13 +0530

    Update AccCal.ino

--- AccCal/AccCal/AccCal.ino
+++ AccCal/AccCal/AccCal.ino
@@ -1,23 +1,19 @@
 
-//--------------------------------------------------------------
+//defining accelerometer directions to pins
 #define xPin A1
 #define yPin A2
 #define zPin A3
 
-//--------------------------------------------------------------
 // Take multiple samples to reduce noise
 const int samples = 10;
-//--------------------------------------------------------------
 
 void setup() 
 {
   Serial.begin(115200);
 }
 
-
 void loop() 
 {
-  //--------------------------------------------------------------
   //Read raw values
   int xRaw=0,yRaw=0,zRaw=0;
   for(int i=0;i<samples;i++)
@@ -27,16 +23,18 @@ void loop()
     zRaw+=analogRead(zPin);
   }
   
+  //Getting average value
   xRaw /= samples;
   yRaw /= samples;
   zRaw /= samples;
-  //--------------------------------------------------------------
+  
+  //Print raw values from accelerometer in serial monitor 
   Serial.print(xRaw);
   Serial.print("\t");
   Serial.print(yRaw);
   Serial.print("\t");
   Serial.print(zRaw);
   Serial.println();
-  //--------------------------------------------------------------
+ 
   delay(200);
 }
